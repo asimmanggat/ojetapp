7b3391bf159de2dd5e9384d0e826e023ff7ae3b3
asimmanggat <asimmanggat@gmail.com>
2022-02-07 00:36:53 +0500

    Form Completed...

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -19,6 +19,8 @@ define(['../accUtils',
       'ojs/ojinputnumber',
       'ojs/ojtable',
       'ojs/ojformlayout',
+      'ojs/ojdatetimepicker',
+      'ojs/ojbutton'
     ],
 
  function(accUtils, ko, $, AsyncLengthValidator) {
@@ -27,9 +29,37 @@ define(['../accUtils',
     
       this._initAllFields();
       this._initValidators();
-      this._initEventListerners();
-    
+      this._initEventListerners(); 
+      this.onInputFirstNameRawValueChanged = this._onInputFirstNameRawValueChanged.bind(this);
+      this.onInputLastNameRawValueChanged = this._onInputLastNameRawValueChanged.bind(this);
+      this.onInputMiddleNameRawValueChanged = this._onInputMiddleNameRawValueChanged.bind(this);
+      this.onSubmitClick = this._onSubmitClick.bind(this);
+      this.onResetClick = this._onResetClick.bind(this);
   }
+
+  
+  //Initialize all observables
+  DashboardViewModel.prototype._initAllFields = function(){
+    this.firstname = ko.observable("");
+    this.lastname = ko.observable("");
+    this.middlename = ko.observable("");
+    this.age = ko.observable(null);
+    this.birthday = ko.observable(null);
+    this.inputFirstNameCustomMessage = ko.observable([]);
+    this.inputLastNameCustomMessage = ko.observable([]);
+    this.inputMiddleNameCustomMessage = ko.observable([]);
+
+
+    this.fullname = ko.computed(function(){
+      if(this.firstname() && this.lastname() && this.middlename()){
+        return this.firstname()+ " " + this.middlename() + " "+ this.lastname();
+      }else{
+        return null;
+      }
+    }, this);   
+  };
+
+
   //Input Validators
   DashboardViewModel.prototype._initValidators = function(){
     //Form Inputs Length Validators
@@ -39,50 +69,71 @@ define(['../accUtils',
         max:20
         }),  
     ]);
-
-  }
- 
-
-  //Initialize all observables
-  DashboardViewModel.prototype._initAllFields = function(){
-    this.firstname = ko.observable("");
-    this.lastname = ko.observable("");
-    this.middlename = ko.observable("");
-    this.age = ko.observable();
-    this.inputCustomMessages = ko.observableArray([]);
-    this.fullname = ko.computed(function(){
-      if(this.firstname() && this.lastname() && this.middlename()){
-        return this.firstname()+ " " + this.middlename() + " "+ this.lastname();
-      }else{
-        return null;
-      }
-    }, this);
-
-    
   };
-
+ 
   //Initialialize all Event Listeners
   DashboardViewModel.prototype._initEventListerners = function(){
-    //On Value changed Validation
-   this.onInputRawValueChanged = function(event){
-     const value = event.detail.value;
-     if(value){
-     event.currentTarget.validate();
-     this.inputCustomMessages([{
-      summary: "Confirmation Message",
-      detail: "Looks Good!",
-      severity: "confirmation",
-    }]);
-       if(value.length >= 2 && value.length <=20){
-         console.log(value);
-         
-       }      
-     }
-   }
+    //On First Name Value changed Validation
+    this._onInputFirstNameRawValueChanged = function(event){
+      const value = event.detail.value;
+      if(value){
+      event.currentTarget.validate();     
+      if(value.length >= 2 && value.length <= 20){
+        this.inputFirstNameCustomMessage([{
+          detail: "Looks Good!",
+          summary: "",
+          severity: "confirmation",
+        }])
+      }
+    }
+  }
+
+    //On Middle Name Value changed Validation
+    this._onInputMiddleNameRawValueChanged = function(event){
+      const value = event.detail.value;
+      if(value){
+      event.currentTarget.validate();     
+      if(value.length >= 2 && value.length <= 20){
+        this.inputMiddleNameCustomMessage([{
+          detail: "Looks Good!",
+          summary: "",
+          severity: "confirmation",
+        }])
+      }
+      }
+    }
+
+    //On Last Name Value changed Validation
+    this._onInputLastNameRawValueChanged = function(event){
+      const value = event.detail.value;
+      if(value){
+      event.currentTarget.validate();     
+      if(value.length >= 2 && value.length <= 20){
+        this.inputLastNameCustomMessage([{
+          detail: "Looks Good!",
+          summary: "",
+          severity: "confirmation",
+        }])
+      }
+      }
+    }
+
+    //On Submit Click
+    this._onSubmitClick = function(){
+      window.alert("Submit");
+    }
+
+    
+    //On Reset Click
+    this._onResetClick = function(){
+    this.firstname(null);
+    this.lastname(null);
+    this.middlename(null);
+    this.age(null);
+    this.birthday(null);
+    }  
  };
 
-
-
-
+    
   return DashboardViewModel;
   });
